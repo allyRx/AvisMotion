824d11a55243a33b542a4ef16099783a1423c9db
allyRx <herindrainyall2003@gmail.com>
2025-04-24 22:54:12 +0300

    create entity

---
+++ src/main/java/com/allyRx/AvisMotion/Entity/Client.java
@@ -0,0 +1,16 @@
+package com.allyRx.AvisMotion.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "CLIENT") @Data @AllArgsConstructor @NoArgsConstructor
+public class Client {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer ID;
+    String email;
+}
