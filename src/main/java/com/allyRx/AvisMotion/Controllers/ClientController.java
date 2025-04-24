50e7add71a7194fc1c466968bd6ae7121a51bb2e
allyRx <herindrainyall2003@gmail.com>
2025-04-25 00:04:33 +0300

    add : repository

--- src/main/java/com/allyRx/AvisMotion/Controllers/ClientController.java
+++ src/main/java/com/allyRx/AvisMotion/Controllers/ClientController.java
@@ -1,17 +1,24 @@
 package com.allyRx.AvisMotion.Controllers;
 
 import com.allyRx.AvisMotion.Entity.Client;
+import com.allyRx.AvisMotion.Service.ClientService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("Client")
+@RequestMapping("Client") @AllArgsConstructor
 public class ClientController {
+
+    //Injection de dependence
+    private ClientService clientService;
 
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
-    public void createClient(@RequestBody Client client) {}
+    public void createClient(@RequestBody Client client) {
+        clientService.creer(client);
+    }
 
 }
--- src/main/java/com/allyRx/AvisMotion/Service/ClientService.java
+++ src/main/java/com/allyRx/AvisMotion/Service/ClientService.java
@@ -1,9 +1,17 @@
 package com.allyRx.AvisMotion.Service;
 
 import com.allyRx.AvisMotion.Entity.Client;
+import com.allyRx.AvisMotion.Repository.ClientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ClientService {
-    public void creer(Client client) {}
+
+    //injection de dependence
+    private ClientRepository clientRepository;
+    public void creer(Client client) {
+        clientRepository.save(client);
+    }
 }
