aae0f0b8c5de668e34cfd9aaed1b19fc9e8862ed
allyRx <herindrainyall2003@gmail.com>
2025-04-24 21:48:49 +0300

    Initial commit

---
+++ src/main/java/com/allyRx/AvisMotion/AvisMotionApplication.java
@@ -0,0 +1,13 @@
+package com.allyRx.AvisMotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisMotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisMotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allyRx/AvisMotion/AvisMotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allyRx.AvisMotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisMotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
