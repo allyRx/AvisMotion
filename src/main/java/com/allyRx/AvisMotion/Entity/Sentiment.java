127d107213fc3dde44001fa0ce91b870e5bc31c5
allyRx <herindrainyall2003@gmail.com>
2025-04-25 22:29:54 +0300

    Create features sentiment

--- src/main/java/com/allyRx/AvisMotion/Entity/Sentiment.java
+++ src/main/java/com/allyRx/AvisMotion/Entity/Sentiment.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.lang.reflect.Type;
-
 import static jakarta.persistence.CascadeType.MERGE;
 import static jakarta.persistence.CascadeType.PERSIST;
 
